02cbc2caca66e419951d686ce72a4e21421ca257
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-10 22:30:29 +0530

    add br

--- src/component/dashboard.tsx
+++ src/component/dashboard.tsx
@@ -15,6 +15,7 @@ export const Dashboard = () => {
       </p>
 
       {/* // TAILWINDCSS */}
+      <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
       <div className="grid bg-neutral-800 grid-cols-2 min-h-fit mt-20 m-28 rounded-3xl ">
         <div className="  flex items-center ">
           <div className=" font-semibold flex ">
@@ -22,8 +23,9 @@ export const Dashboard = () => {
               <div className="">
                 <SiTailwindcss size={44} className="text-sky-400 mx-5 " />
               </div>
-              <p className="text-3xl ">
+              <p className="text-3xl">
                 Tailwind CSS
+                <br />
                 <span className="font-normal   text-gray-400 tracking-wide text-base mt-1">
                   Rapidly build modern websites without ever leaving your
                   HTML.Tailwind is unapologetically modern, and takes advantage
@@ -42,9 +44,10 @@ export const Dashboard = () => {
             src="https://tailwindcss.com"
             title="Tailwind CSS Dashboard"
             className="w-full h-full"
-          ></iframe>
+            ></iframe>
         </div>
       </div>
+            </a>
 
       {/* UI */}
       <div className="grid bg-neutral-800 grid-cols-2 min-h-fit mt-20 m-28 rounded-3xl ">
@@ -61,6 +64,7 @@ export const Dashboard = () => {
               </div>
               <p className="text-3xl pl-10">
                 Aceternity UI
+                <br />
                 <span className="font-normal   text-gray-400 tracking-wide text-base mt-1">
                   Make your websites look 10x better. Copy paste the most
                   trending components and use them in your websites without
@@ -94,6 +98,7 @@ export const Dashboard = () => {
               </div>
               <p className="text-3xl pl-10">
                 Google Fonts
+                <br />
                 <span className="font-normal   text-gray-400 tracking-wide text-base mt-1">
                   Create stunning typography, generate CSS, and find
                   inspiration. What are you waiting for?
@@ -113,7 +118,7 @@ export const Dashboard = () => {
       {/* // Framer */}
 
       <div className="grid bg-neutral-800 grid-cols-2 min-h-fit mt-20 m-28 rounded-3xl ">
-        <div className="  flex items-center ">
+        <div className="flex items-center ">
           <div className=" font-semibold flex ">
             <div className=" flex ">
               <div className="">
@@ -126,6 +131,7 @@ export const Dashboard = () => {
               </div>
               <p className="text-3xl pl-10">
                 Unsplash
+                <br />
                 <span className="font-normal   text-gray-400 tracking-wide text-base mt-1">
                   Yes, it’s really free. All images can be downloaded and used
                   for personal or commercial projects. The internet’s source for
@@ -156,7 +162,7 @@ export const Dashboard = () => {
                 />
               </div>
               <p className="text-3xl pl-10">
-                ICONS8
+                ICONS8<br />
                 <span className="font-normal   text-gray-400 tracking-wide text-base mt-1">
                  Original stock graphics by our team, plus design apps and AI tools. The ultimate design kit for creatives and developers
                 </span>
@@ -178,7 +184,7 @@ export const Dashboard = () => {
           <div className=" font-semibold flex ">
             <div className=" flex ">
               <p className="text-3xl pl-20">
-                Coolors
+                Coolors<br />
                 <span className="font-normal   text-gray-400 tracking-wide text-base mt-1">
                   The super fast color palettes generators. Create the perfect
                   pallete or get inspired by thousands of beautifull color
