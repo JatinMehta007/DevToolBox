40221473f00b0b9c62e7becfaeb030d6721f11df
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-10 22:10:19 +0530

    Add utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import type { ClassValue } from "clsx"; // ✅ type-only import
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(...inputs));
+}
