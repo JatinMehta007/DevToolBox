9463935c8d7d0e66f2fa67e92d1a2f4359653fae
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-12 16:43:24 +0530

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Dashboard } from "./component/dashboard"
+import { Dashboard } from "./pages/dashboard";
+import { WobbleCardDemo } from "./component/Icons";
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Dashboard></Dashboard>}></Route>
+      <Route path="/icons" element={<WobbleCardDemo></WobbleCardDemo>}></Route>
     </Routes>
     </BrowserRouter>
     </>
