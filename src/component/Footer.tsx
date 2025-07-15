9cf6c49aaf0cbbd5035d720e1f6dcbe74a485ec4
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-15 18:46:35 +0530

    add footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,7 +1,10 @@
 export const Footer = () => {
     return(
-        <div className="h-20   bg-neutral-800">
-            DevToolBox © Copyright 2025. All rights reserved
+        <div className="h-20 text-base   bg-stone-800 text-center text-stone-400  flex-col justify-center items-center mt-10 pt-4 ">
+            <p>
+            DevToolbox © Copyright 2025. All rights reserved
+            </p>
+            <p>Design & Developed by <a href="https://portfolio-jatinmehta.vercel.app/"><span className="text-blue-500 font-bold">Jatin</span></a></p>
         </div>
     )
 }
