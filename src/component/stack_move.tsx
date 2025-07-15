d4b0a89ec5f281d8401eec23710e55d30e04f7e9
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-15 18:48:45 +0530

    add logos

--- src/component/stack_move.tsx
+++ src/component/stack_move.tsx
@@ -1,123 +1,177 @@
 "use client";
 
 import { InfiniteMovingCards } from "../ui/moving_stack";
-import stack1 from "/accer.png";
-import stack2 from "/upslash.png";
-import stack3 from "/googel.svg";
-import stack4 from "/hero.png";
-import stack5 from "/react.png";
-import stack6 from "/github.png";
-import stack7 from "/tailwindcss.svg";
-import stack8 from "/shadcn.svg";
-import stack9 from "/figma.png";
-import stack10 from "/framer.png";
-import stack11 from "/material-ui.svg";
-import stack12 from "/bootstrap-.svg";
-import stack13 from "/lottieflies.png";
 
 export function InfiniteMovingCardsDemo() {
   return (
-    <div className=" rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
+    <div className="rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
-        items={testimonials}
-        direction="left"
-        speed="slow"
-      />
-       <div className="mt-4">
-       <InfiniteMovingCards
         items={testimonials}
         direction="right"
         speed="slow"
+      />
+      <div className="">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="left"
+          speed="slow"
         />
-        </div>
+      </div>
     </div>
   );
 }
 
 const testimonials = [
   {
-    quote:
-    <img src={stack1} alt="accertinity " className="w-28" />, 
-},
-{
-    quote:
-    <img src={stack2} alt="upslash" className="w-28" />, 
-},
-{
-    quote:
-    <img src={stack3} alt="google_font" className="w-32" />, 
-},
-{
-    quote:
-    <img src={stack4} alt="google_font" className="w-32" />, 
-},
+    quote: <img src="/logos/accer.png" alt="accertinity" className="w-28" />,
+  },
+  {
+    quote: <img src="/logos/upslash.png" alt="upslash" className="w-28" />,
+  },
+  {
+    quote: <img src="/logos/googel.svg" alt="google_font" className="w-32" />,
+  },
+  {
+    quote: <img src="/logos/hero.png" alt="hero" className="w-32" />,
+  },
+  {
+    quote: <img src="/logos/react.png" alt="react" className="w-32" />,
+  },
+  {
+    quote: <img src="/logos/github.png" alt="github" className="w-32" />,
+  },
+  {
+    quote: <img src="/logos/tailwindcss.svg" alt="tailwind" className="w-32" />,
+  },
+  {
+    quote: (
+      <img
+        src="https://goodies.icons8.com/web/common/header/logo/logo-icons8.svg"
+        alt="icons8"
+        className="w-28"
+      />
+    ),
+  },
+  {
+    quote: <img src="/logos/shadcn.svg" alt="shadcn" className="w-32" />,
+  },
+  {
+    quote: <img src="/logos/figma.png" alt="figma" className="w-32" />,
+  },
+  {
+    quote: <img src="/logos/framer.png" alt="framer" className="w-32" />,
+  },
+  {
+    quote: (
+      <img src="/logos/material-ui.svg" alt="material-ui" className="w-32" />
+    ),
+  },
+  {
+    quote: <img src="/logos/bootstrap-.svg" alt="bootstrap" className="w-32" />,
+  },
+  {
+    quote: (
+      <img
+        src="https://typescale.com/wp-content/themes/typescale/_site/images/logo_header.svg"
+        alt="typescale"
+        className="w-32"
+      />
+    ),
+  },
+  {
+    quote: (
+      <img
+        src="https://www.realtimecolors.com/favicon.png"
+        alt="realtime-colors"
+        className="w-32"
+      />
+    ),
+  },
+  {
+    quote: (
+      <img
+        src="https://huemint.com/assets/img/logo-icon.svg"
+        alt="huemint"
+        className="w-28"
+      />
+    ),
+  },
+  {
+    quote: (
+      <img
+        src="https://lucide.dev/logo.dark.svg"
+        alt="lucide icons"
+        className="w-28"
+      />
+    ),
+  },
+  {
+    quote: (
+      <img
+        src="https://coolors.co/assets/img/logo.svg"
+        alt="typescale"
+        className="w-44"
+      />
+    ),
+  },
+  {
+    quote: (
+      <img
+        src="https://flowbite.com/images/logo.svg"
+        alt="flowbite"
+        className="w-28"
+      />
+    ),
+  },
+  
+  {
+    quote: (
+      <img
+      src="https://neon.com/_next/static/svgs/6da928883916f39a4848774319dcaf81.svg"
+      alt="typescale"
+      className="w-32"
+      />
+    ),
+  },
+  {
+    quote: (
+      <img
+        src="/logos/lottieflies.png"
+        alt="lottieflies"
+        className="w-32"
+      />
+    ),
+  },
+  {
+    quote: (
+      <img
+        src="https://aiven.io/assets/logo-aiven-DLhBz3IO.svg"
+        alt="typescale"
+        className="w-44"
+      />
+    ),
+  },
+  {
+    quote: <img src="/logos/vercel.png" alt="vercel" className="w-32" />,
+  },
 
-{
-    quote:
-    <img src={stack5} alt="react" className="w-32" />, 
-},
+  {
+    quote: <img src="/logos/netlify.webp" alt="netlify" className="w-32" />,
+  },
 
-{
-    quote:
-    <img src={stack6} alt="github" className="w-32" />, 
-},
+  {
+    quote: <img src="/logos/postman.png" alt="postman" className="w-32" />,
+  },
 
-{
-    quote:
-    <img src={stack7} alt="tailwind" className="w-32" />, 
-},
-{
-    quote:
-    <img src="https://goodies.icons8.com/web/common/header/logo/logo-icons8.svg" alt="icons" className="w-28" />, 
-},
-{
-    quote:
-    <img src={stack8} alt="shadcn" className="w-32" />, 
-},
-{
-    quote:
-    <img src={stack9} alt="figma" className="w-32" />, 
-},
-{
-    quote:
-    <img src={stack10} alt="framer" className="w-32" />, 
-},
-{
-    quote:
-    <img src={stack11} alt="material-ui" className="w-32" />, 
-},
+  {
+    quote: <img src="/logos/cloudflare.png" alt="cloudflare" className="w-44" />,
+  },
 
-{
-    quote:
-    <img src={stack12} alt="boostrap" className="w-32" />, 
-},
+  {
+    quote: <img src="/logos/removebg.png" alt="removebg" className="w-32" />,
+  },
 
-{
-    quote:
-    <img src="https://typescale.com/wp-content/themes/typescale/_site/images/logo_header.svg" alt="typescale" className="w-32" />, 
-},
-
-{
-    quote:
-    <img src="https://www.realtimecolors.com/favicon.png" alt="realtime-colors" className="w-32" />, 
-},
-
-{
-    quote:
-    <img src="https://huemint.com/assets/img/logo-icon.svg" alt="huemint" className="w-28" />, 
-},
-{
-    quote:
-    <img src="https://lucide.dev/logo.dark.svg" alt="lucide icons" className="w-28" />, 
-},
-{
-    quote:
-    <img src="https://flowbite.com/images/logo.svg" alt="lucide icons" className="w-28" />, 
-},
-
-{
-    quote:
-    <img src={stack13} alt="boostrap" className="w-32" />, 
-},
-
+  {
+    quote: <img src="/logos/canva.png" alt="canva" className="w-32" />,
+  }
 ];
