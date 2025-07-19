cc98ec7c49aacb1fd2acdac3a6528f79c8023440
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-19 13:35:57 +0530

    make reponsiveness

--- src/ui/moving_stack.tsx
+++ src/ui/moving_stack.tsx
@@ -65,7 +65,7 @@ export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
       <ul
         ref={scrollerRef}
         className={cn(
-          "flex w-max min-w-full shrink-0 h-[200px] flex-nowrap py-4",
+          "flex w-max min-w-full shrink-0 h-[180px] md:h-[200px] flex-nowrap md:py-4",
           start && "animate-scroll",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
@@ -76,7 +76,7 @@ export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
         {items.map((item, idx) => (
           <li
             key={idx}
-            className="relative shrink-0 w-[150px] flex items-center justify-center"
+            className="relative shrink-0 w-[100px] md:w-[150px] flex items-center justify-center"
           >
             <span className="relative z-20 text-sm font-normal">
               {item.quote}
