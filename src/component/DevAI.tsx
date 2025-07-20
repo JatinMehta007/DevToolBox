b7f712a9acb1cf74f79aeec5beb4119d7f4e7ac5
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-20 10:31:13 +0530

    make reponsive

--- src/component/DevAI.tsx
+++ src/component/DevAI.tsx
@@ -26,8 +26,8 @@ export function DevAI() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div className="max-w-xs">
-                <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              <div className="max-w-base md:max-w-base md:max-w-xs ">
+                <h2 className="text-left text-balance text-xl sm:text-2xl md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
                  ChatGPT
                 </h2>
                 <p className="mt-4 text-left  text-base/6 text-neutral-200">
@@ -49,10 +49,10 @@ export function DevAI() {
           </WobbleCard>
            <WobbleCard containerClassName="col-span-1 min-h-[450px] md:min-h-[300px] p-3 border border-gray-700 md:border-0 bg-blue-800 relative w-full -3 md:p-0 md:right-24 md:w-[600px]">
             <a href="https://gemini.google.com/app?hl=en-IN" target="_blank" rel="noopener noreferrer">
-            <h2 className="max-w-96  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            <h2 className="max-w-96  text-left text-balance text-xl md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Gemini
             </h2>
-            <p className="mt-4 max-w-[18rem] text-left  text-base/6 text-neutral-200">
+            <p className="mt-4 max-w-base md:max-w-[18rem] text-left  text-base/6 text-neutral-200">
             Meet Gemini, your personal AI assistant. AI for every developer
             </p>
             <p className="text-white/90 font-medium inline-flex items-center mt-5 gap-2">
@@ -81,8 +81,8 @@ export function DevAI() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div className="max-w-xs">
-                <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              <div className="max-w-base md:max-w-xs">
+                <h2 className="text-left text-balance text-xl md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
                  Github Copilot
                 </h2>
                 <p className="mt-4 text-left  text-base/6 text-neutral-200">
@@ -104,10 +104,10 @@ export function DevAI() {
           </WobbleCard>
            <WobbleCard containerClassName="col-span-1 min-h-[450px] md:min-h-[300px] p-3 border border-gray-700 md:border-0 bg-blue-700 relative w-full -3 md:p-0 md:right-24 md:w-[600px]">
             <a href="https://www.meta.ai/" target="_blank" rel="noopener noreferrer">
-            <h2 className="max-w-96  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            <h2 className="max-w-96  text-left text-balance text-xl md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Meta
             </h2>
-            <p className="mt-4 max-w-[18rem] text-left  text-base/6 text-neutral-200">
+            <p className="mt-4 max-w-base md:max-w-[18rem] text-left  text-base/6 text-neutral-200">
            Use Meta AI assistant to get things done, create AI-generated images for free, and get answers to any of your questions.
             </p>
             <p className="text-white/90 font-medium inline-flex items-center mt-5 gap-2">
@@ -138,8 +138,8 @@ export function DevAI() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div className="max-w-xs">
-                <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              <div className="max-w-base md:max-w-xs">
+                <h2 className="text-left text-balance text-xl md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
                  Claude
                 </h2>
                 <p className="mt-4 text-left  text-base/6 text-neutral-200">
@@ -161,10 +161,10 @@ Claude is a next generation AI assistant built by Anthropic and trained to be sa
           </WobbleCard>
            <WobbleCard containerClassName="col-span-1 min-h-[450px] md:min-h-[300px] p-3 border border-gray-700 md:border-0 bg-gray-800 relative w-full -3 md:p-0 md:right-24 md:w-[600px]">
             <a href="https://cursor.com/" target="_blank" rel="noopener noreferrer">
-            <h2 className="max-w-96  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            <h2 className="max-w-96  text-left text-balance text-xl md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Cursor
             </h2>
-            <p className="mt-4 max-w-[18rem] text-left  text-base/6 text-neutral-200">
+            <p className="mt-4 max-w-base md:max-w-[18rem]  text-left  text-base/6 text-neutral-200">
            The AI Code Editor. Built to make you extraordinarily productive, Cursor is the best way to code with AI.
             </p>
             <p className="text-white/90 font-medium inline-flex items-center mt-5 gap-2">
