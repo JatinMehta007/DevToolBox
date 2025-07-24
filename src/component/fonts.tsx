69ce41afa946ef3ba79bbc0aca9034315de26a38
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-25 00:01:51 +0530

    make responsive

--- src/component/fonts.tsx
+++ src/component/fonts.tsx
@@ -13,7 +13,7 @@ export function Fonts() {
         <div className="grid grid-cols-1  lg:grid-cols-3 gap-4 mx-auto  relative px-1 sm:px-10 md:px-20 lg:px-0 lg:right-14">
 
           <WobbleCard 
-            containerClassName="col-span-1 lg:col-span-2 border border-gray-700 lg:border-0  p-3 lg:p-0 h-full bg-blue-800 w-full lg:w-[75%] min-h-[500px] lg:min-h-[300px]"
+            containerClassName="col-span-1 lg:col-span-2 border border-gray-700 lg:border-0  p-3 lg:p-0 h-full bg-blue-800 w-full lg:w-[70%] xl:w-[75%] min-h-[500px] lg:min-h-[300px]"
             className=""
             >
           <a href="https://fonts.google.com/" target="_blank" rel="noopener noreferrer">
@@ -34,11 +34,11 @@ export function Fonts() {
               width={500}
               height={500}
               alt="linear demo image"
-              className="absolute -right-4 lg:-right-[30%] grayscale filter -bottom-20 lg:-bottom-10 object-contain rounded-2xl"
+              className="absolute -right-4 lg:-right-[45%] xl:-right-[30%] grayscale filter lg:-bottom-28 xl:-bottom-10 object-contain rounded-2xl"
               />
               </a>
           </WobbleCard>
-          <WobbleCard containerClassName="col-span-1 min-h-[450px] lg:min-h-[300px] p-3 border border-gray-700 lg:border-0 bg-amber-800 relative w-full -3 lg:p-0 lg:right-24 lg:w-[600px]">
+          <WobbleCard containerClassName="col-span-1 min-h-[450px] lg:min-h-[300px] p-3 border border-gray-700 lg:border-0 bg-amber-800 relative w-full -3 lg:p-0 lg:right-24 lg:w-[480px] xl:w-[590px]">
             <a href="https://www.fontshare.com/" target="_blank" rel="noopener noreferrer">
             <h2 className="max-w-96  text-left text-balance text-xl sm:text-2xl md:text-3xl  lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Fontshare
@@ -56,7 +56,7 @@ export function Fonts() {
               width={500}
               height={500}
               alt="linear demo image"
-              className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl"
+              className="absolute -right-4  lg:-right-[60%] xl:-right-[40%] grayscale filter lg:-bottom-28 xl:-bottom-16 md:-bottom-10 object-contain rounded-2xl"
               />
               </a>
           </WobbleCard>
@@ -66,7 +66,7 @@ export function Fonts() {
 <div className="grid grid-cols-1  lg:grid-cols-3 gap-4 mx-auto  relative px-1 sm:px-10 md:px-20 lg:px-0 lg:right-14">
 
           <WobbleCard 
-            containerClassName="col-span-1 lg:col-span-2 border border-gray-700 lg:border-0  p-3 lg:p-0 h-full bg-pink-800 w-full lg:w-[75%] min-h-[500px] lg:min-h-[300px]"
+            containerClassName="col-span-1 lg:col-span-2 border border-gray-700 lg:border-0  p-3 lg:p-0 h-full bg-pink-800 w-full lg:w-[70%] xl:w-[75%]min-h-[500px] lg:min-h-[300px]"
             className=""
             >
           <a href="https://fonts.adobe.com/" target="_blank" rel="noopener noreferrer">
@@ -89,11 +89,11 @@ Find the perfect font. Premium fonts (paid Adobe Creative Cloud)
               width={500}
               height={500}
               alt="linear demo image"
-              className="absolute -right-4 lg:-right-[30%] grayscale filter -bottom-10 object-contain rounded-2xl"
+              className="absolute -right-4 lg:-right-[45%] xl:-right-[30%] grayscale filter lg:-bottom-28 xl:-bottom-10 object-contain rounded-2xl"
               />
               </a>
           </WobbleCard>
-          <WobbleCard containerClassName="col-span-1 min-h-[450px] lg:min-h-[300px] p-3 border border-gray-700 lg:border-0 bg-sky-700 relative w-full -3 lg:p-0 lg:right-24 lg:w-[600px]">
+          <WobbleCard containerClassName="col-span-1 min-h-[450px] lg:min-h-[300px] p-3 border border-gray-700 lg:border-0 bg-sky-700 relative w-full -3 lg:p-0 lg:right-24 lg:w-[480px] xl:w-[590px]">
             <a href="https://typescale.com/" target="_blank" rel="noopener noreferrer">
             <h2 className="max-w-96  text-left text-balance text-xl sm:text-2xl md:text-3xl  lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Typescale
@@ -111,7 +111,7 @@ Find the perfect font. Premium fonts (paid Adobe Creative Cloud)
               width={500}
               height={500}
               alt="linear demo image"
-              className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-20 md:-bottom-10 object-contain rounded-2xl"
+              className="absolute -right-4  lg:-right-[60%] xl:-right-[40%] grayscale filter lg:-bottom-28 xl:-bottom-16 md:-bottom-10 object-contain rounded-2xl"
               />
               </a>
           </WobbleCard>
@@ -122,7 +122,7 @@ Find the perfect font. Premium fonts (paid Adobe Creative Cloud)
 <div className="grid grid-cols-1  lg:grid-cols-3 gap-4 mx-auto  relative px-1 sm:px-10 md:px-20 lg:px-0 lg:right-14">
 
           <WobbleCard 
-            containerClassName="col-span-1 lg:col-span-2 border border-gray-700 lg:border-0  p-3 lg:p-0 h-full bg-emerald-800 w-full lg:w-[75%] min-h-[500px] lg:min-h-[300px]"
+            containerClassName="col-span-1 lg:col-span-2 border border-gray-700 lg:border-0  p-3 lg:p-0 h-full bg-emerald-800 w-full lg:w-[70%] xl:w-[75%] min-h-[500px] lg:min-h-[300px]"
             className=""
             >
           <a href="https://fontjoy.com/" target="_blank" rel="noopener noreferrer">
@@ -144,11 +144,11 @@ Find the perfect font. Premium fonts (paid Adobe Creative Cloud)
               width={500}
               height={500}
               alt="linear demo image"
-              className="absolute -right-4 lg:-right-[30%] grayscale filter -bottom-1 md:-bottom-10 object-contain rounded-2xl"
+              className="absolute -right-4 lg:-right-[45%] xl:-right-[30%] grayscale filter lg:-bottom-28 xl:-bottom-10 object-contain rounded-2xl"
               />
               </a>
           </WobbleCard>
-          <WobbleCard containerClassName="col-span-1 min-h-[450px] lg:min-h-[300px] p-3 border border-gray-700 lg:border-0 bg-black-80 relative w-full -3 lg:p-0 lg:right-24 lg:w-[600px]">
+          <WobbleCard containerClassName="col-span-1 min-h-[450px] lg:min-h-[300px] p-3 border border-gray-700 lg:border-0 bg-black-80 relative w-full -3 lg:p-0 lg:right-24 lg:w-[480px] xl:w-[590px]">
             <a href="https://uncut.wtf/" target="_blank" rel="noopener noreferrer">
             <h2 className="max-w-96  text-left text-balance text-xl sm:text-2xl md:text-3xl  lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Uncut
@@ -166,7 +166,7 @@ Find the perfect font. Premium fonts (paid Adobe Creative Cloud)
               width={500}
               height={500}
               alt="linear demo image"
-              className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-20 lg:-bottom-10 object-contain rounded-2xl"
+              className="absolute -right-4  lg:-right-[60%] xl:-right-[40%] grayscale filter lg:-bottom-28 xl:-bottom-16 md:-bottom-10 object-contain rounded-2xl"
               />
               </a>
           </WobbleCard>
@@ -178,7 +178,7 @@ Find the perfect font. Premium fonts (paid Adobe Creative Cloud)
 <div className="grid grid-cols-1  lg:grid-cols-3 gap-4 mx-auto  relative px-1 sm:px-10 md:px-20 lg:px-0 lg:right-14">
 
           <WobbleCard 
-            containerClassName="col-span-1 lg:col-span-2 border border-gray-700 lg:border-0  p-3 lg:p-0 h-full bg-[#A27EAF]   w-full lg:w-[75%] min-h-[500px] lg:min-h-[300px]"
+            containerClassName="col-span-1 lg:col-span-2 border border-gray-700 lg:border-0  p-3 lg:p-0 h-full bg-[#A27EAF]   w-full lg:w-[70%] xl:w-[75%] min-h-[500px] lg:min-h-[300px]"
             className=""
           >
           <a href="https://www.fontsquirrel.com/" target="_blank" rel="noopener noreferrer">
@@ -200,11 +200,11 @@ Find the perfect font. Premium fonts (paid Adobe Creative Cloud)
               width={500}
               height={500}
               alt="linear demo image"
-              className="absolute -right-4 lg:-right-[30%] grayscale filter -bottom-10 object-contain rounded-2xl"
+              className="absolute -right-4 lg:-right-[45%] xl:-right-[30%] grayscale filter lg:-bottom-28 xl:-bottom-10 object-contain rounded-2xl"
               />
               </a>
           </WobbleCard>
-          <WobbleCard containerClassName="col-span-1 min-h-[450px] lg:min-h-[300px] p-3 border border-gray-700 lg:border-0 bg-indigo-800 relative w-full -3 lg:p-0 lg:right-24 lg:w-[600px]">
+          <WobbleCard containerClassName="col-span-1 min-h-[450px] lg:min-h-[300px] p-3 border border-gray-700 lg:border-0 bg-indigo-800 relative w-full -3 lg:p-0 lg:right-24 lg:w-[480px] xl:w-[590px]">
             <a href="https://velvetyne.fr/" target="_blank" rel="noopener noreferrer">
             <h2 className="max-w-96  text-left text-balance text-xl sm:text-2xl md:text-3xl  lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Velvetyne
@@ -222,7 +222,7 @@ Find the perfect font. Premium fonts (paid Adobe Creative Cloud)
               width={500}
               height={500}
               alt="linear demo image"
-              className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-20 md:-bottom-10 object-contain rounded-2xl"
+              className="absolute -right-4  lg:-right-[60%] xl:-right-[40%] grayscale filter lg:-bottom-28 xl:-bottom-16 md:-bottom-10 object-contain rounded-2xl"
               />
               </a>
           </WobbleCard>
