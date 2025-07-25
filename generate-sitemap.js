b7ab1b553321f48e1d380e2324d6a79732b3bb3a
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-25 13:27:29 +0530

    generate sitemap

---
+++ generate-sitemap.js
@@ -0,0 +1,25 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+
+const sitemap = new SitemapStream({ hostname: 'https://dev-tool-box-jatin.vercel.app/' });
+const writeStream = createWriteStream('./public/sitemap.xml');
+
+// List your routes here
+const routes = ['/',
+  '/icons',
+  '/fonts',
+  '/colors',
+  '/ui',
+  '/tools',
+  '/logos',
+  '/commands',
+  '/ai',];
+
+routes.forEach((url) => sitemap.write({ url }));
+sitemap.end();
+
+streamToPromise(sitemap).then((sm) => {
+  writeStream.write(sm.toString());
+  writeStream.end();
+  console.log(' Sitemap successfully generated at /public/sitemap.xml');
+});
